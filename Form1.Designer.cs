a53c47b9801b5a36a39c48458743f2701158167e
Volodymyr Shafran <volodymyr.shafran98@gmail.com>
2021-11-17 13:16:03 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace LabEx
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DataGridView = new System.Windows.Forms.DataGridView();
+            this.ButtonSave = new System.Windows.Forms.Button();
+            this.ButtonOpen = new System.Windows.Forms.Button();
+            this.LabelRows = new System.Windows.Forms.Label();
+            this.ButtonAddRow = new System.Windows.Forms.Button();
+            this.ButtonDelRow = new System.Windows.Forms.Button();
+            this.LabelColumns = new System.Windows.Forms.Label();
+            this.ButtonAddColumn = new System.Windows.Forms.Button();
+            this.ButtonDelColumn = new System.Windows.Forms.Button();
+            this.TextBoxExpression = new System.Windows.Forms.TextBox();
+            this.ButtonCalcExp = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DataGridView
+            // 
+            this.DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView.Location = new System.Drawing.Point(-3, 41);
+            this.DataGridView.Name = "DataGridView";
+            this.DataGridView.RowTemplate.Height = 25;
+            this.DataGridView.Size = new System.Drawing.Size(843, 411);
+            this.DataGridView.TabIndex = 0;
+            // 
+            // ButtonSave
+            // 
+            this.ButtonSave.Location = new System.Drawing.Point(93, 7);
+            this.ButtonSave.Name = "ButtonSave";
+            this.ButtonSave.Size = new System.Drawing.Size(75, 28);
+            this.ButtonSave.TabIndex = 1;
+            this.ButtonSave.Text = "Save";
+            this.ButtonSave.UseVisualStyleBackColor = true;
+            // 
+            // ButtonOpen
+            // 
+            this.ButtonOpen.Location = new System.Drawing.Point(12, 7);
+            this.ButtonOpen.Name = "ButtonOpen";
+            this.ButtonOpen.Size = new System.Drawing.Size(75, 28);
+            this.ButtonOpen.TabIndex = 2;
+            this.ButtonOpen.Text = "Open";
+            this.ButtonOpen.UseVisualStyleBackColor = true;
+            // 
+            // LabelRows
+            // 
+            this.LabelRows.AutoSize = true;
+            this.LabelRows.Location = new System.Drawing.Point(217, 14);
+            this.LabelRows.Name = "LabelRows";
+            this.LabelRows.Size = new System.Drawing.Size(35, 15);
+            this.LabelRows.TabIndex = 3;
+            this.LabelRows.Text = "Rows";
+            // 
+            // ButtonAddRow
+            // 
+            this.ButtonAddRow.Location = new System.Drawing.Point(258, 7);
+            this.ButtonAddRow.Name = "ButtonAddRow";
+            this.ButtonAddRow.Size = new System.Drawing.Size(28, 28);
+            this.ButtonAddRow.TabIndex = 4;
+            this.ButtonAddRow.Text = "+";
+            this.ButtonAddRow.UseVisualStyleBackColor = true;
+            // 
+            // ButtonDelRow
+            // 
+            this.ButtonDelRow.Location = new System.Drawing.Point(295, 7);
+            this.ButtonDelRow.Name = "ButtonDelRow";
+            this.ButtonDelRow.Size = new System.Drawing.Size(28, 28);
+            this.ButtonDelRow.TabIndex = 5;
+            this.ButtonDelRow.Text = "-";
+            this.ButtonDelRow.UseVisualStyleBackColor = true;
+            // 
+            // LabelColumns
+            // 
+            this.LabelColumns.AutoSize = true;
+            this.LabelColumns.Location = new System.Drawing.Point(373, 14);
+            this.LabelColumns.Name = "LabelColumns";
+            this.LabelColumns.Size = new System.Drawing.Size(55, 15);
+            this.LabelColumns.TabIndex = 6;
+            this.LabelColumns.Text = "Columns";
+            // 
+            // ButtonAddColumn
+            // 
+            this.ButtonAddColumn.Location = new System.Drawing.Point(434, 7);
+            this.ButtonAddColumn.Name = "ButtonAddColumn";
+            this.ButtonAddColumn.Size = new System.Drawing.Size(28, 28);
+            this.ButtonAddColumn.TabIndex = 7;
+            this.ButtonAddColumn.Text = "+";
+            this.ButtonAddColumn.UseVisualStyleBackColor = true;
+            // 
+            // ButtonDelColumn
+            // 
+            this.ButtonDelColumn.Location = new System.Drawing.Point(471, 7);
+            this.ButtonDelColumn.Name = "ButtonDelColumn";
+            this.ButtonDelColumn.Size = new System.Drawing.Size(28, 28);
+            this.ButtonDelColumn.TabIndex = 8;
+            this.ButtonDelColumn.Text = "-";
+            this.ButtonDelColumn.UseVisualStyleBackColor = true;
+            // 
+            // TextBoxExpression
+            // 
+            this.TextBoxExpression.Location = new System.Drawing.Point(542, 11);
+            this.TextBoxExpression.Name = "TextBoxExpression";
+            this.TextBoxExpression.Size = new System.Drawing.Size(203, 23);
+            this.TextBoxExpression.TabIndex = 9;
+            // 
+            // ButtonCalcExp
+            // 
+            this.ButtonCalcExp.Location = new System.Drawing.Point(751, 7);
+            this.ButtonCalcExp.Name = "ButtonCalcExp";
+            this.ButtonCalcExp.Size = new System.Drawing.Size(75, 28);
+            this.ButtonCalcExp.TabIndex = 10;
+            this.ButtonCalcExp.Text = "Calculate";
+            this.ButtonCalcExp.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(838, 450);
+            this.Controls.Add(this.ButtonCalcExp);
+            this.Controls.Add(this.TextBoxExpression);
+            this.Controls.Add(this.ButtonDelColumn);
+            this.Controls.Add(this.ButtonAddColumn);
+            this.Controls.Add(this.LabelColumns);
+            this.Controls.Add(this.ButtonDelRow);
+            this.Controls.Add(this.ButtonAddRow);
+            this.Controls.Add(this.LabelRows);
+            this.Controls.Add(this.ButtonOpen);
+            this.Controls.Add(this.ButtonSave);
+            this.Controls.Add(this.DataGridView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LabEx";
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DataGridView;
+        private System.Windows.Forms.Button ButtonSave;
+        private System.Windows.Forms.Button ButtonOpen;
+        private System.Windows.Forms.Label LabelRows;
+        private System.Windows.Forms.Button ButtonAddRow;
+        private System.Windows.Forms.Button ButtonDelRow;
+        private System.Windows.Forms.Label LabelColumns;
+        private System.Windows.Forms.Button ButtonAddColumn;
+        private System.Windows.Forms.Button ButtonDelColumn;
+        private System.Windows.Forms.TextBox TextBoxExpression;
+        private System.Windows.Forms.Button ButtonCalcExp;
+    }
+}
+
